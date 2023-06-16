29fc416ebc1aa399b8b1f2c2d3e13a7668971597
Maxim Pechalov <magaterkolg@gmail.com>
2023-06-16 18:51:58 +0300

    ex2

---
+++ ex2/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+
+Console.WriteLine("coordinates of the first point: ");
+Console.Write("x = ");
+int ax = int.Parse(Console.ReadLine());
+
+Console.Write("y = ");
+int ay = int.Parse(Console.ReadLine());
+
+Console.Write("z = ");
+int az = int.Parse(Console.ReadLine());
+
+Console.WriteLine("coordinates of the second point: ");
+Console.Write("x = ");
+int bx = int.Parse(Console.ReadLine());
+
+Console.Write("y = ");
+int by = int.Parse(Console.ReadLine());
+
+Console.Write("z = ");
+int bz = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Distance = {Math.Sqrt(Math.Pow(ax - bx, 2) + Math.Pow(ay - by, 2)+ Math.Pow(az + bz, 2)):f2}");
